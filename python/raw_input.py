4fd0d7de0f8786995004e57df09ccec8209e2792
wanghan <wanghan@longcheer.com>
2017-03-25 03:53:51 +0800

    add raw_input.py & utf-8.py

---
+++ python/raw_input.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python 
+
+raw_input("\n\n Press the enter key to exit.")
---
+++ python/utf-8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
+#单行注释用#号
+
+'''
+多行注释
+'''
+
+"""
+多
+行
+注
+释
+"""
+
+#python中，同一行多个语句的分隔用分号
+import sys; x='runoob'; sys.stdout.write(x + '\n')
+
+
+print('中文显示')
+print("显示中文")
