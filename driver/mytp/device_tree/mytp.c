115b523e59b66eeebd8dbd5e894fcd452e2ede30
daoshuti <daoshuti@163.com>
2017-06-09 20:34:08 +0800

    [driver][mytp][device tree] mytp.c delete <linux/device.h>

--- driver/mytp/device_tree/mytp.c
+++ driver/mytp/device_tree/mytp.c
@@ -9,7 +9,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
-#include <linux/device.h>
 #include <linux/of_gpio.h>
 /****************************************************************************
  * Define {{{1 
