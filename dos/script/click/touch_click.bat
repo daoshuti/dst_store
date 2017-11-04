10a03f49e9cbaaf2fc32f434d525fa318b066e84
wanghan <daoshuti@163.com>
2017-11-04 17:20:35 +0800

    [dos][script] add touch_click.bat

---
+++ dos/script/click/touch_click.bat
@@ -0,0 +1,17 @@
+adb shell sendevent /dev/input/event2 1 172 1
+adb shell sendevent /dev/input/event2 0 0 0
+
+
+adb shell sendevent /dev/input/event2 1 139 1
+adb shell sendevent /dev/input/event2 0 0 0
+
+
+adb shell sendevent /dev/input/event2 3 53 231
+adb shell sendevent /dev/input/event2 0 0 0
+
+
+
+adb shell sendevent /dev/input/event2 1 172 0
+
+adb shell sendevent /dev/input/event2 1 139 0
+adb shell sendevent /dev/input/event2 0 0 0
