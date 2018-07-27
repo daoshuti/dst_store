61f47ec8dd539da9ea9f9d2ba469528eda4f16ad
wanghan <daoshuti@163.com>
2018-07-27 09:44:14 +0800

    [c_test] print.c 同行回显(使用stdout buffer可以做更多有意思的东西)

--- c_test/print.c
+++ c_test/print.c
@@ -1,8 +1,21 @@
+/*************************************************************************
+ @Author: wanghan
+ @Created Time : Mon 23 Jul 2018 02:39:16 PM CST
+ @File Name: print.c
+ @Description:
+ ************************************************************************/
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, const char *argv[])
 {
-	int a=1,b=2,c=3;
-	printf("hello%d""你好%d""world%d\n",a,b,c);
-	return 0;
+    int i;
+    char string_line[1024] = {};
+    for(i=0;i<=100;i++){
+        sprintf(string_line,"\r%02d percent",i);
+        printf(string_line);
+        fflush(stdout);
+        sleep(1);
+    }
+    return 0;
 }
