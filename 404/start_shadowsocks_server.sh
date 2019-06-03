75a11e8759e132f88cb6edff6a7ccf5ec58787c1
daoshuti <daoshuti@163.com>
2019-06-03 02:54:11 +0000

    add 404 script

---
+++ 404/start_shadowsocks_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author: wanghan
+# Created Time : 2018年07月02日 星期一 11时49分20秒
+# File Name: start_shadowsocks.sh
+# Description:
+
+BASE_PATH=$(cd `dirname $0`; pwd)
+LOG_PATH=${BASE_PATH}/my_shadowsocks_server.log
+echo The shadowsocks config file is ${BASE_PATH}/shadowsocks.json.
+echo Save log in ${LOG_PATH}.
+if [ -f ${BASE_PATH}/shadowsocks.json ]; then
+    setsid ssserver -c ${BASE_PATH}/shadowsocks.json > ${LOG_PATH} 2>&1 &
+    #sslocal -c ${BASE_PATH}/shadowsocks.json
+    if [ $? != 0 ]; then
+        echo Failure to the sslocal execute!!!
+        exit 1
+    fi
+else
+    echo "The '${BASE_PATH}/shadowsocks.json' file does not exist!"
+    exit 78 #EX_CONFIG
+fi
+
+echo Done.
+
